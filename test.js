7d283db562745c89496734cb4120bade43d69ec2
lathe31 <66665160+lathe31@users.noreply.github.com>
2023-03-26 13:57:40 +0530

    added script for cdn testing

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-
+const get_mpv_string = (videos_links_list)=>{
+    let base_string = `mpv `;
+    let links_string=``;
+    for(let i of videos_links_list){
+        links_string += `"${i}" `;
+    }
+    return base_string+links_string;
+};
+const kaboom = ()=>{
+    const files_element_list = document.querySelectorAll("div.list-group-item > a:nth-child(3)");
+    const files_link_list = [];
+    files_element_list.forEach((x)=>files_link_list.push(x.href));
+    const video_files_link_list = files_link_list.filter((x)=>x.endsWith("mp4"));
+    const mpv_string = get_mpv_string(video_files_link_list);
+    navigator.clipboard.writeText(mpv_string);
+    console.log(mpv_string);
+    alert("Links Copied!");
+};
+window.addEventListener("load", (event) => {
+    console.log("page is fully loaded");
+});
